e02b53fc5c7a68430f148554c47e6b2797dba65e
Josh (Castawhat) <school@slitro.studio>
2026-03-09 14:54:24 +1300

    Coin works!

--- area_3d.gd
+++ area_3d.gd
@@ -3,7 +3,11 @@ extends Area3D
 @export var value: int = 1
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("Player"):
 		print("Player detected!") # Check the Output console for this!
 		GameManager.add_score(value)
 		queue_free()
+
+func _process(delta: float) -> void:
+	# 90 degrees per second
+	rotate_y(deg_to_rad(90) * delta)
